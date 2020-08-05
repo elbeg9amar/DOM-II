c487ff6da6ad9d1ba3547d3758c93f0681d6dbc7
Amarbayasgalan elbeg <elbeg9amar@yahoo.com>
2020-08-05 12:57:59 -0400

    used mouseover on text and img

--- js/index.js
+++ js/index.js
@@ -1,3 +1,35 @@
 // Your code goes here
-const allLinks = document.querySelectorAll('a')
-const allImg = document.querySelectorAll('img')
+const logoOver = document.querySelector('.logo-heading')
+
+logoOver.addEventListener('select', (event) => {
+    return event.target.style.fontSize = "20px";
+
+    event.preventDefault ()
+});
+logoOver.addEventListener('click', (event) => {
+    return event.target.style.fontSize = "60px";
+});
+
+const allParag = document.querySelectorAll('p, img');
+allParag.forEach(index => {
+    index.addEventListener('mouseover',(event) => {
+         event.target.style.fontFamily = 'Indie Flower', 'cursive';
+    });
+});
+
+const allImg = document.querySelectorAll('img');
+allImg.forEach(index => {
+    index.addEventListener('mouseover',(event) => {
+        console.log(event.target.style)
+            event.target.style.borderColor = 'green';
+            event.target.style.border = '1rem';
+            event.target.style.borderStyle = 'solid';
+    });
+});
+
+const allHeader = document.querySelectorAll('h1, h2, h4');
+allHeader.forEach(index =>{
+    index.addEventListener('mouseover',(event) => {
+        event.target.style.fontSize = '5rem';
+    });
+});
