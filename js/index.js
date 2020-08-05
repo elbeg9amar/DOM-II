964a87f5da9abdd12a268b2656eb63349d0da06b
Amarbayasgalan elbeg <elbeg9amar@yahoo.com>
2020-08-05 17:36:35 -0400

    completed project

--- js/index.js
+++ js/index.js
@@ -1,35 +1,71 @@
 // Your code goes here
 const logoOver = document.querySelector('.logo-heading')
 
-logoOver.addEventListener('select', (event) => {
-    return event.target.style.fontSize = "20px";
-
-    event.preventDefault ()
-});
 logoOver.addEventListener('click', (event) => {
+    return event.target.style.fontSize = "20px";
+});
+logoOver.addEventListener('mouseover', (event) => {
     return event.target.style.fontSize = "60px";
 });
 
-const allParag = document.querySelectorAll('p, img');
+const allParag = document.querySelectorAll('p');
 allParag.forEach(index => {
     index.addEventListener('mouseover',(event) => {
          event.target.style.fontFamily = 'Indie Flower', 'cursive';
     });
+    index.addEventListener('mouseleave',(event) => {
+        event.target.style = 'initial';
+   });
 });
 
 const allImg = document.querySelectorAll('img');
 allImg.forEach(index => {
     index.addEventListener('mouseover',(event) => {
-        console.log(event.target.style)
-            event.target.style.borderColor = 'green';
+            event.target.style.borderColor = 'black';
             event.target.style.border = '1rem';
             event.target.style.borderStyle = 'solid';
+    });
+    index.addEventListener('mouseleave', (event) => {
+        event.target.style = 'initial';
+    });
+});
+
+allImg.forEach(el => {
+    el.addEventListener('keydown', (event) => {
+        event.target.style.maxWidth = '110%'
     });
 });
 
 const allHeader = document.querySelectorAll('h1, h2, h4');
 allHeader.forEach(index =>{
     index.addEventListener('mouseover',(event) => {
-        event.target.style.fontSize = '5rem';
+        return event.target.style.fontSize = '5rem';
+    });
+    index.addEventListener('mouseleave',(event) => {
+        event.target.style = 'initial';
     });
 });
+
+const allLinks = document.querySelectorAll('a');
+allLinks.forEach(index => {
+    index.addEventListener('focus', (event) =>{ 
+        console.log(event)
+        event.target.style.fontSize = '2.5rem'
+        event.target.style.color = 'solid black';
+    });
+    index.addEventListener('mouseleave', (event) =>{ 
+        event.target.style = 'initial';
+    });
+});
+
+
+
+const signUp = document.querySelectorAll('.btn');
+const allText = document.all;
+signUp.forEach(index => {
+    index.addEventListener('click', () =>{
+        window.addEventListener('dblclick', () => {
+            console.log('Iam here')
+        });
+    });
+})
